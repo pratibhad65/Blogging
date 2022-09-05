3b98ca4e31ec6e71c24b88e373e111efa26c9d9e
AnujTembhurne <you@example.com>
2022-09-06 00:12:12 +0530

    new

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,17 +1,21 @@
 const blogModel = require('../models/blogModel')
+const authorModel = require('../models/authorModel');
 
-const createBlog = async function(req, res) {
+const createBlog = async function (req, res) {
     try {
-    let author = req.body
-    if(!author) {
-        res.send("Author is not Present");
+        let data = req.body
+        let ndata = data.authorId
+        let validation = await authorModel.findById(ndata)
+        if (!validation) {
+            res.send({ status: false, msg: " author is not present" })
+        }
+        if (data.isPublished) data.publishedAt = new Date()
+        if (data.isDeleted) data.deletedAt = new Date()
+
+        let savedData = await blogModel.create(author);
+        res.status(201).send({ status: true, msg: savedData })
+    } catch (err) {
+        res.status(400).send({ msg: err.message })
     }
-    if(author.isPublished) author.publishedAt=new Date()
-    if(author.isDeleted) author.deletedAt=new Date()
-    let savedData = await blogModel.create(author);
-    res.status(201).send({status:true , msg: savedData})
-}catch(err){
-    res.status(400).send({msg: err.message})
-}
 }
 module.exports.createBlog = createBlog
